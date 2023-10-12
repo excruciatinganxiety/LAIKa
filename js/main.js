c6c25e2b43ac5ff6e7d28c46b745797dc0e10162
excruciatinganxiety <147346398+excruciatinganxiety@users.noreply.github.com>
2023-10-12 13:15:54 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+btn.addEventListener('click', ()=>{
+    nav.classList.toggle('menu-open');
+
+
+
+
+})};
